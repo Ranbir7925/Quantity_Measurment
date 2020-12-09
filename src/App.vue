b5e456c44b0a11e803e716c9670668549aa9d88f
Ranbir Singh <honeyranbir7925@gmail.com>
2020-12-09 23:14:14 +0530

    add:App bar component

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <AppBar/>
+  </div>
+</template>
+
+<script>
+import AppBar from './components/AppBar'
+export default {
+  name: 'app',
+  components:{
+    AppBar
+  }
+}
+</script>
+
+<style>
+#app{
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
---
+++ src/components/AppBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="div">
+    <p>Welcome to Quantity Measurment</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppBar",
+};
+</script>
+
+<style >
+.div {
+  background-color: #446dff;
+  text-align: center;
+  padding-top: 2%;
+  padding-bottom: 2%;
+}
+.div p {
+  font-size: xx-large;
+  color: white;
+}
+</style>
