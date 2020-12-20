49124337d9e49065cfaf85a37b3d1640be5454e2
Ranbir Singh <honeyranbir7925@gmail.com>
2020-12-20 20:20:35 +0530

    refactor:to show sub units data from parents component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,31 @@
 <template>
   <div id="app">
-    <AppBar/>
+    <AppBar />
+    <Conversion />
   </div>
 </template>
 
 <script>
-import AppBar from './components/AppBar'
+import AppBar from "./components/AppBar";
+import Conversion from "./components/Conversion";
 export default {
-  name: 'app',
-  components:{
-    AppBar
-  }
-}
+  name: "app",
+  components: {
+    AppBar,
+    Conversion,
+  },
+};
 </script>
 
 <style>
-#app{
+* {
+  margin: 0;
+  padding: 0;
+}
+body {
+  background-color: #f4f5f7;
+}
+#app {
   font-family: Arial, Helvetica, sans-serif;
 }
 </style>
